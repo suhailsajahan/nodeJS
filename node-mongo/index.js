126ac0ab31b4a67bcb6f2606202b7f041c1c032e
Suhail <suhailsajahan@gmail.com>
2021-04-07 12:37:33 +0530

    Node mongo ex1

---
+++ node-mongo/index.js
@@ -0,0 +1,38 @@
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');       //makes various checks
+
+const url = 'mongodb://localhost:27017/';
+const dbname = 'conFusion';
+
+MongoClient.connect(url, (err, client) => {
+
+    assert.equal(err,null);
+
+    console.log('Connected correctly to server');
+
+    const db = client.db(dbname);        //connect to the DB
+
+    const collection = db.collection("dishes");
+
+    collection.insertOne({"name": "Uthappizza", "description": "test"},         //insert one document (1st argument)
+    (err, result) => {
+        assert.equal(err,null);
+
+        console.log("After Insert:\n");
+        console.log(result.ops);
+
+        collection.find({}).toArray((err, docs) => {
+            assert.equal(err,null);
+            
+            console.log("Found:\n");
+            console.log(docs);
+
+            db.dropCollection("dishes", (err, result) => {
+                assert.equal(err,null);
+
+                client.close();
+            });
+        });
+    });
+
+});
